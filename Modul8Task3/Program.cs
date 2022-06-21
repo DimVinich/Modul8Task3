678d868bf4733c3482897a322265ee1118fc0190
Dmitrij Sokolov <DmitrijVS@gmail.com>
2022-06-21 16:59:03 +0300

    Добавьте файлы проекта.

---
+++ Modul8Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Modul8Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = @"D:\Temp";             //  отладочная информация, затереть перед сдачей
+            long folderSize = 0;
+
+            // Ввод и провека папки на существование
+            Console.WriteLine("Введите директорию для расчёта занимаемого места :");
+            folder = Console.ReadLine();
+
+            if (!Directory.Exists(folder))
+            {
+                Console.WriteLine(" Вы ввели не существующую директорию. Программа прекращает свою работу.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
